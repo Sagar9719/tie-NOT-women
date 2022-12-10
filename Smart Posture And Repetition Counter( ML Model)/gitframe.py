e81769e01b1e4405e0432ddd4cf8c6e872ebaf93
Sagar Singh <92724493+Sagar9719@users.noreply.github.com>
2022-12-10 21:47:58 +0530

    final

---
+++ Smart Posture And Repetition Counter( ML Model)/gitframe.py
@@ -0,0 +1,11 @@
+import cv2
+def gen_frames():  
+    while True:
+        success, frame = camera.read()  # read the camera frame
+        if not success:
+            break
+        else:
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
